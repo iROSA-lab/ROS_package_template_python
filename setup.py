2ad2456adea32930a94ff5bfcd4dd5295f543413
Alonso94 <ay20-5-1994@hotmail.com>
2023-03-07 09:19:47 +0000

    package template

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
+
+# make the scripts in the scripts folder executable
+import sys
+import os
+for script in os.listdir('scripts'):
+    os.chmod(os.path.join('scripts', script), 509)
+
 d = generate_distutils_setup(
     packages=['package_name'],
     package_dir={'': 'src'}
